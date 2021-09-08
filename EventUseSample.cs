cbe2809461e483518931a852e3d6db717389b41a
shun-shun123 <shunsuke15vb@gmail.com>
2021-09-09 00:53:24 +0900

    delegateについてちょっと詳しくなった

--- EventUseSample.cs
+++ EventUseSample.cs
@@ -5,10 +5,17 @@ namespace EventReferenceTest
     public class EventUseSample
     {
         public event Action SampleEvent;
+
+        public delegate void SampleDelegate();
         
         public void Execute()
         {
             SampleEvent?.Invoke();
+        }
+
+        public void ExecuteDelegate(SampleDelegate sampleDelegate)
+        {
+            sampleDelegate();
         }
     }
 }
